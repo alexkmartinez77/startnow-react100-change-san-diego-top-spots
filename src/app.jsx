a23f3f72abf6ee2490f8c4c6ab825da4697b127a
Alex <alexkmartinez77@gmail.com>
2018-05-16 08:50:34 -0700

    Uploading update files on 5-16-18

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpot from './topspot';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        topspots: []
+    };
+}
+
+
+
+componentWillMount(){
+  axios
+     .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+     .then(function(response){
+       const topspots = response.data;
+       this.setState({ topspots: topspots })
+     }.bind(this))
+
+  //  equivalent to:
+  // .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+  // .then(response => response.data)
+  // .then(topspots => this.setState({ topspots: topspots }));
+  }
+
   render() {
     return (
-      <div className='App'></div>
+      <div className='App'>
+        <div className='jumbotron'>
+          <h1>San Diego Top Spots</h1>
+           <p>A list of the top 30 places to see in San Diego, California. </p>
+        </div>
+        { 
+        this.state.topspots.map(topspot => (
+        <TopSpot
+            key={topspot.id}
+            name={topspot.name}
+            description={topspot.description}
+            location={topspot.location} />
+    ))
+        }
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,6 @@ import { render } from 'react-dom';
 
 import App from './app';
 
+
+
 render(<App />, document.getElementById('root'));
